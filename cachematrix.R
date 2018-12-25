625007a3bf64e7e70c544e8f6d2f12b99cf2d528
AtukuriVyshnavi <45869612+AtukuriVyshnavi@users.noreply.github.com>
2018-12-25 11:02:58 +0530

    "modified cachematrix.R"

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -17,6 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+# This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
